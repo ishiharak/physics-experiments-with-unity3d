50e95d9ed5112e987a162140e6d934b652dde141
Kengo Ishihara <ken-ishihara@sch.ed.city.kobe.jp>
2017-11-14 23:36:30 +0900

    Update Jisshu78

---
+++ Jisshu7/Assets/DrawLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+    public GameObject[] Vectors;
+
+    void Start() {
+        this.GetComponent<LineRenderer>().positionCount = Vectors.Length;
+    }
+
+    void Update () {
+        for (int i = 0; i < Vectors.Length; i++) {
+			this.GetComponent<LineRenderer>().SetPosition(i, Vectors[i].transform.position);
+        }
+	}
+}
