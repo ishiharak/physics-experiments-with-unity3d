f2ac2f1d8b1eadae22f92d40270ee17bfaad062d
Kengo Ishihara <ken-ishihara@sch.ed.city.kobe.jp>
2017-11-23 21:21:44 +0900

    Update EarthAndMoon Simulation

--- Jisshu8/Assets/UniversalGravitation.cs
+++ Jisshu8/Assets/UniversalGravitation.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UniversalGravitation : MonoBehaviour {
+    public GameObject target;
     public float G;
     public float m;
-    public GameObject target;
-    Vector3 v;
+    public Vector3 v;
 
 	void Start () {
-        Debug.Log(G * 60 * 60 * 24 * 60 * 60 * 24);
-        v = Vector3.zero;
+        
 	}
 	
-	void Update () {
+	void FixedUpdate () {
+        Debug.Log(Time.time);
         Vector3 s = this.transform.position;
         float r = (target.transform.position - this.transform.position).magnitude;
         float M = target.GetComponent<UniversalGravitation>().m;
         Vector3 a = (G * M / (r * r)) * (target.transform.position - this.transform.position).normalized;
-        //Debug.Log(a);
         v = v + a * Time.deltaTime;
         s = s + v * Time.deltaTime;
         this.transform.position = s;
