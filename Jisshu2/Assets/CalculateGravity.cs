99ad776051ca8eb2ad990431e80e917cf33a6756
Kengo Ishihara <ken-ishihara@sch.ed.city.kobe.jp>
2018-04-25 21:00:06 +0900

    Update Jisshu2

---
+++ Jisshu2/Assets/CalculateGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate () {
+		Debug.Log(Time.time);
+		Debug.Log(this.transform.position.y);
+	}
+}
---
+++ Jisshu2/Assets/DisplayY.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate () {
+        //if (Input.GetKeyDown(???)) {
+        //    Debug.Log(???);
+        //}
+	}
+}
