517b78eb473efdd8df848cacd4845bc1175ae07c
Kengo Ishihara <ken-ishihara@sch.ed.city.kobe.jp>
2019-10-08 14:20:40 +0900

    インデントを修正

--- Jisshu5/Assets/MoveBackAndForth.cs
+++ Jisshu5/Assets/MoveBackAndForth.cs
@@ -13,13 +13,13 @@ public class MoveBackAndForth : MonoBehaviour
     void Update()
     {
         //if (Time.time ???) {
-        //x = x + ??? * Mathf.Deg2Rad * Time.deltaTime;
-        this.transform.position = new Vector3(3f * Mathf.Sin(x), 0f, 0f);
-        //if (this.transform.position.x ???) {
-        //	this.GetComponent<MeshRenderer> ().material.color = Color.red;
-        //} else {
-        //	this.GetComponent<MeshRenderer> ().material.color = Color.blue;
-        //}
+        //    x = x + ??? * Mathf.Deg2Rad * Time.deltaTime;
+        //    this.transform.position = new Vector3(3f * Mathf.Sin(x), 0f, 0f);
+        //    if (this.transform.position.x ???) {
+        //    	this.GetComponent<MeshRenderer> ().material.color = Color.red;
+        //    } else {
+        //    	this.GetComponent<MeshRenderer> ().material.color = Color.blue;
+        //    }
         //}
     }
 }
