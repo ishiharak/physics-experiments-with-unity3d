4ad8cef91139b59cb5d69df2d76090a912f22b0a
Kengo Ishihara <ken-ishihara@sch.ed.city.kobe.jp>
2020-11-10 14:13:15 +0900

    課題6のプログラム修正&例3,4のRigidbody設定変更

--- Jisshu6/Assets/Projectile.cs
+++ Jisshu6/Assets/Projectile.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float v, angle, g;
+    public float v0, angle, g;
     float vx, vy;
 
     void Start()
     {
-        vx = v * Mathf.Cos(angle * Mathf.Deg2Rad);
-        vy = v * Mathf.Sin(angle * Mathf.Deg2Rad);
+        vx = v0 * Mathf.Cos(angle * Mathf.Deg2Rad);
+        vy = v0 * Mathf.Sin(angle * Mathf.Deg2Rad);
     }
 
     void FixedUpdate()
